aef411f2d2b70635093881994407d78043a0496e
phenrique <ph.cardoso10@gmail.com>
2024-12-16 11:14:18 -0300

    Add Mqtt Manager

---
+++ src/MqttManager/MqttManager.cpp
@@ -0,0 +1,103 @@
+#include "MqttManager.h"
+
+TimerHandle_t mqttReconnectTimer;
+AsyncMqttClient mqttClient;
+
+void connectToMqtt();
+void onMqttPublish(uint16_t packetId);
+void onMqttSubscribe(uint16_t packetId, uint8_t qos);
+void onMqttUnsubscribe(uint16_t packetId);
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+
+void connectToMqtt()
+{
+    Serial.println("Conectando ao servidor MQTT...");
+    mqttClient.connect();
+}
+
+MqttManager::MqttManager(const char *clientId, const char *host, uint16_t port)
+    : clientId(clientId), host(host), port(port)
+{
+}
+
+void MqttManager::begin(AsyncMqttClientInternals::OnConnectUserCallback onMqttConnect, AsyncMqttClientInternals::OnDisconnectUserCallback onMqttDisconnect)
+{
+
+    mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+
+    mqttClient.setServer(host, port);
+    mqttClient.setClientId(clientId);
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onSubscribe(onMqttSubscribe);
+    mqttClient.onUnsubscribe(onMqttUnsubscribe);
+    mqttClient.onMessage(onMqttMessage);
+    mqttClient.onPublish(onMqttPublish);
+}
+
+void MqttManager::connect()
+{
+    connectToMqtt();
+}
+
+void MqttManager::reconnect()
+{
+    xTimerStart(mqttReconnectTimer, 0);
+}
+
+void MqttManager::disableReconnection()
+{
+    xTimerStop(mqttReconnectTimer, 0);
+}
+
+uint16_t MqttManager::publish(const char *topic, uint8_t qos, bool retain, const char *payload)
+{
+    return mqttClient.publish(topic, qos, retain, payload);
+}
+
+uint16_t MqttManager::subscribe(const char *topic, uint8_t qos)
+{
+    return mqttClient.subscribe(topic, qos);
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos)
+{
+    Serial.println("Inscrição reconhecida.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+    Serial.print("  qos: ");
+    Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId)
+{
+    Serial.println("Inscrição cancelada.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+}
+
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
+{
+    Serial.println("Publish received.");
+    Serial.print("  topic: ");
+    Serial.println(topic);
+    Serial.print("  qos: ");
+    Serial.println(properties.qos);
+    Serial.print("  dup: ");
+    Serial.println(properties.dup);
+    Serial.print("  retain: ");
+    Serial.println(properties.retain);
+    Serial.print("  len: ");
+    Serial.println(len);
+    Serial.print("  index: ");
+    Serial.println(index);
+    Serial.print("  total: ");
+    Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId)
+{
+    Serial.println("Publish acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,24 @@
 
 #include "ble.h"
 #include "WifiManager/WifiManager.h"
+#include "MqttManager/MqttManager.h"
 extern "C"
 {
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
 }
-#include <AsyncMqttClient.h>
 #include <ArduinoJson.h>
 #include "DHT.h"
 #include "NoiseSensor/NoiseSensor.h"
+
+#define MQTT_MESSAGE_LEN 128
+
+#define MQTT_HOST "200.239.66.45"
+#define MQTT_PORT 1883
+#define MQTT_CLIENT_ID "testEsp32Device-01" // cada dispositivo deve ter um id diferente
+#define MQTT_TOPIC_ROOT "ESP32PhcnTeste"
+#define SENSORS_TOPIC MQTT_TOPIC_ROOT "/sensors"
+#define NOISE_TOPIC MQTT_TOPIC_ROOT "/noises"
 
 bool deviceConnected = false;
 
@@ -19,6 +28,7 @@ Preferences preferences;
 uint32_t value = 0;
 
 WifiManager wifiManager;
+MqttManager mqttManager(MQTT_CLIENT_ID, MQTT_HOST, MQTT_PORT);
 
 Ble ble = Ble(preferences);
 
@@ -36,21 +46,11 @@ long deviceId;
 int noiseThreshold;
 boolean active = false;
 
-#define MQTT_HOST "200.239.66.45"
-#define MQTT_PORT 1883
-#define MQTT_CLIENT_ID "testEsp32Device-01" // cada dispositivo deve ter um id diferente
-#define MQTT_TOPIC_ROOT "ESP32PhcnTeste"
-#define MQTT_MESSAGE_LEN 128
-#define SENSORS_TOPIC MQTT_TOPIC_ROOT "/sensors"
-#define NOISE_TOPIC MQTT_TOPIC_ROOT "/noises"
-
 DHT dht(DHTPIN, DHTTYPE);
 
 StaticJsonDocument<MQTT_MESSAGE_LEN> doc;
 StaticJsonDocument<64> docNoise;
 
-AsyncMqttClient mqttClient;
-TimerHandle_t mqttReconnectTimer;
 TimerHandle_t sensorsTimer;
 TimerHandle_t noiseTimer;
 
@@ -68,15 +68,8 @@ void setNoiseThreshold()
   preferences.end();
 }
 
-void connectToMqtt()
-{
-  Serial.println("Conectando ao servidor MQTT...");
-  mqttClient.connect();
-}
-
 void activeReadSensors()
 {
-
   Serial.println("Iniciando leitura dos sensores");
   active = true;
   xTimerStart(noiseTimer, 0);
@@ -85,7 +78,6 @@ void activeReadSensors()
 
 void deactiveReadSensors()
 {
-
   Serial.println("Interrompendo leitura dos sensores");
   active = false;
   xTimerStop(noiseTimer, 0); // desativa as leituras caso o Wi-Fi esteja desconectado
@@ -110,7 +102,7 @@ void noiseMonitoring()
     serializeJson(docNoise, output);
     Serial.print("noise up! ");
     Serial.println(output);
-    mqttClient.publish(NOISE_TOPIC, 0, true, output);
+    mqttManager.publish(NOISE_TOPIC, 0, false, output);
     xTimerStart(noiseTimer, 0);
   }
 
@@ -137,7 +129,7 @@ void publishWeatherRead()
     char output[MQTT_MESSAGE_LEN];
     serializeJson(doc, output);
     Serial.println(output);
-    mqttClient.publish(SENSORS_TOPIC, 0, true, output);
+    mqttManager.publish(SENSORS_TOPIC, 0, false, output);
   }
 }
 
@@ -150,13 +142,13 @@ void WiFiEvent(WiFiEvent_t event)
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    connectToMqtt();
+    mqttManager.connect();
 
     break;
   case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
     Serial.println("WiFi lost connection");
     wifiManager.disconnect();          // Precisamos limpar a conexão antes de tentar reconectar
-    xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+    mqttManager.disableReconnection(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
     active = false;                    // desativa as leituras caso o Wi-Fi esteja desconectado
     wifiManager.reconnect();           // Espera o timer definido e depois tenta reconectar
     break;
@@ -168,18 +160,17 @@ void onMqttConnect(bool sessionPresent)
   Serial.println("Connected to MQTT.");
   Serial.print("Session present: ");
   Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
+  uint16_t packetIdSub = mqttManager.subscribe("test/lol", 2);
   Serial.print("Subscribing at QoS 2, packetId: ");
   Serial.println(packetIdSub);
-  mqttClient.publish("test/lol", 0, true, "test 1");
+  mqttManager.publish("test/lol", 0, true, "test 1");
   Serial.println("Publishing at QoS 0");
-  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+  uint16_t packetIdPub1 = mqttManager.publish("test/lol", 1, true, "test 2");
   Serial.print("Publishing at QoS 1, packetId: ");
   Serial.println(packetIdPub1);
-  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+  uint16_t packetIdPub2 = mqttManager.publish("test/lol", 2, true, "test 3");
   Serial.print("Publishing at QoS 2, packetId: ");
   Serial.println(packetIdPub2);
-  mqttClient.subscribe("phtest/lol", 2);
 
   xTimerStart(sensorsTimer, 0);
 }
@@ -190,52 +181,10 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
   Serial.println("Motivo:");
   deactiveReadSensors();
 
-  if (WiFi.isConnected())
+  if (wifiManager.isConnected())
   {
-    xTimerStart(mqttReconnectTimer, 0);
+    mqttManager.reconnect();
   }
-}
-
-void onMqttSubscribe(uint16_t packetId, uint8_t qos)
-{
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
-}
-
-void onMqttUnsubscribe(uint16_t packetId)
-{
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
-{
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-}
-
-void onMqttPublish(uint16_t packetId)
-{
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
 }
 
 void setup()
@@ -254,20 +203,11 @@ void setup()
   noiseSensor.begin();
   noiseSensor.beginSmoothing();
 
+  mqttManager.begin(onMqttConnect, onMqttDisconnect);
   wifiManager.begin(WiFiEvent);
 
-  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   sensorsTimer = xTimerCreate("sensorsTimer", pdMS_TO_TICKS(10000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(activeReadSensors));
   noiseTimer = xTimerCreate("noiseTimer", pdMS_TO_TICKS(1000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(noiseMonitoring));
-
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  mqttClient.setClientId(MQTT_CLIENT_ID);
 
   setDeviceId();
 
