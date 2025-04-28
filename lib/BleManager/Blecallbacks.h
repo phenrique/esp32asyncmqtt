6f364dc21065240ae87dbec18338e15b11c626a9
phenrique <ph.cardoso10@gmail.com>
2025-04-28 19:22:26 -0300

    Refatoração do recebiemento de info do dispositivo pelo ble

--- lib/BleManager/Blecallbacks.h
+++ lib/BleManager/Blecallbacks.h
@@ -35,7 +35,7 @@ class CredentialsCallbacks : public BLECharacteristicCallbacks {
 
     int separatorIndex = value.indexOf(':');
     if (separatorIndex == -1) {
-      Serial.println("Invalid format");
+      Serial.println("Formato do payload ble inválido");
       return;
     }
 
@@ -112,55 +112,62 @@ class DevInfoCallbacks : public BLECharacteristicCallbacks {
   Preferences preferences;
   uint8_t controller;
   
-  void onWrite(BLECharacteristic *pCharacteristic) {
-
+  void onWrite(BLECharacteristic *pCharacteristic) override {
     String value = pCharacteristic->getValue();
 
-    long deviceId = std::strtoul(value.c_str(), NULL, 0);
+    Serial.print("Recebido BLE: ");
+    Serial.println(value);
 
-    if(controller == 1){
-      if(deviceId > 0){
-        preferences.begin("info", false);
-        preferences.putLong("deviceId", deviceId);
-
-        Serial.print("deviceId received: ");
-        Serial.println(deviceId);
-        Serial.println("deviceId Saved using Preferences");
-
-        preferences.end();
-        pCharacteristic->setValue("");
-      }
-      controller = 0;
-    }
-    if(controller == 2){
-      preferences.begin("info", false);
-      preferences.putInt("noiseThreshold", std::strtoul(value.c_str(), NULL, 0));
-
-      Serial.print("noiseThreshold received: ");
-      Serial.println(value.c_str());
-      Serial.println("noiseThreshold Saved using Preferences");
-
-      preferences.end();
-      pCharacteristic->setValue("");
-      controller = 0;
-    }
-    if(controller == 3){
-      preferences.begin("info", false);
-      preferences.putString("deviceName", value.c_str());
-
-      Serial.print("deviceName received: ");
-      Serial.println(value.c_str());
-      Serial.println("deviceName Saved using Preferences");
-
-      preferences.end();
-      pCharacteristic->setValue("");
-      controller = 0;
+    // Verifica o formato cmd:data
+    int separatorIndex = value.indexOf(':');
+    if (separatorIndex == -1) {
+        Serial.println("Formato do payload BLE inválido");
+        return;
     }
 
-    controller = std::strtoul(value.c_str(), NULL, 0);
-    Serial.print("valor do controlador");
-    Serial.println(controller);
-  }
+    // Extrai o comando (cmd) e os dados (data)
+    int cmd = value.substring(0, separatorIndex).toInt();
+    String data = value.substring(separatorIndex + 1);
+    data.trim();
+
+    preferences.begin("info", false);
+
+    switch (cmd) {
+        case 1: // Salvar deviceId
+        {
+            long deviceId = std::strtoul(data.c_str(), NULL, 0);
+            if (deviceId > 0) {
+                preferences.putLong("deviceId", deviceId);
+                Serial.print("deviceId recebido e salvo: ");
+                Serial.println(deviceId);
+            } else {
+                Serial.println("deviceId inválido");
+            }
+            break;
+        }
+        case 2: // Salvar noiseThreshold
+        {
+            int noiseThreshold = std::strtoul(data.c_str(), NULL, 0);
+            preferences.putInt("noiseThreshold", noiseThreshold);
+            Serial.print("noiseThreshold recebido e salvo: ");
+            Serial.println(noiseThreshold);
+            break;
+        }
+        case 3: // Salvar deviceName
+        {
+            preferences.putString("deviceName", data.c_str());
+            Serial.print("deviceName recebido e salvo: ");
+            Serial.println(data);
+            break;
+        }
+        default:
+            Serial.println("Comando inválido");
+            break;
+    }
+
+    preferences.end();
+    pCharacteristic->setValue(""); // Limpa o valor da característica
+}
 
   void onRead(BLECharacteristic *pCharacteristic) {
 
